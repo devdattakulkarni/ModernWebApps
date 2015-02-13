62c67f9e12b34981ca8018287defac41ea9e077f
Devdatta Kulkarni <devdatta.kulkarni@rackspace.com>
2015-02-13 10:18:12 -0600

    Reading port from command line; Socket timeout

--- HTTP/TestClientSocket.java
+++ HTTP/TestClientSocket.java
@@ -15,20 +15,25 @@ public class TestClientSocket {
 	public static void main(String []args) {
 		
 		Socket client = null;
+		FileOutputStream f = null;
+
 		try {
-			
+		f = new FileOutputStream("serveroutput.txt");		     
+
 			if (args.length < 2) {
 				System.out.println("Usage:");
-				System.out.println("java TestClientSocket <host> <resource>");
+				System.out.println("java TestClientSocket <host> <port> <resource>");
 				System.exit(0);
 			}
 			
 			String host = args[0];
-			String resource = args[1];
-			
-			int portToUse = 80;
+			String port = args[1];
+			String resource = args[2];
+
+			int portToUse = Integer.valueOf(port.trim());
 			
 			client = new Socket(host, portToUse);
+                        client.setSoTimeout(1000);
 
 			OutputStream out = client.getOutputStream();
 			PrintWriter pout = new PrintWriter(out);
@@ -41,7 +46,7 @@ public class TestClientSocket {
 			byte[] buffer = new byte[200];
 			int n = - 1;
 			
-			FileOutputStream f = new FileOutputStream("serveroutput.txt");
+
 			
 			while ( (n = in.read(buffer)) != -1)
 			{
@@ -51,7 +56,9 @@ public class TestClientSocket {
 			        f.write(buffer, 0, n);
 			    }
 			}
-			f.close();
+			if (f != null) {
+			    f.close();
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 			try {
@@ -59,6 +66,15 @@ public class TestClientSocket {
 			} catch (IOException e1) {
 				e1.printStackTrace();
 			}
-		}		
+		}
+		catch (Exception e) {
+		    if (f != null) {
+			try {
+			    f.close();
+			} catch (Exception e1) {
+			    e1.printStackTrace();
+			}
+		    }
+		}
 	}
 }
