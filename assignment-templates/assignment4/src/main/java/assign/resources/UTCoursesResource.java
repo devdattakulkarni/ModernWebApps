ef64e74147ff7517fb3032fe96d45368bfe8d607
Devdatta Kulkarni <devdattakulkarni@gmail.com>
2018-11-02 16:02:55 -0500

    Assignment 4 template updated

--- assignment-templates/assignment4/src/main/java/assign/resources/UTCoursesResource.java
+++ assignment-templates/assignment4/src/main/java/assign/resources/UTCoursesResource.java
@@ -6,11 +6,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletContext;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.StreamingOutput;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -18,39 +24,88 @@ import javax.xml.bind.Marshaller;
 
 import assign.domain.Course;
 import assign.domain.Courses;
+import assign.domain.NewCourse;
 import assign.domain.NotFound;
-import assign.domain.Project;
-import assign.domain.Projects;
 import assign.services.CourseStudentService;
 import assign.services.CourseStudentServiceImpl;
 
-@Path("/listing")
+@Path("/")
 public class UTCoursesResource {
-	
+
 	CourseStudentService courseStudentService;
 	String password;
 	String username;
-	String dburl;	
-	
+	String dburl;
+	String host;
+	String dbname;
+
 	public UTCoursesResource(@Context ServletContext servletContext) {
-		dburl = servletContext.getInitParameter("DBURL");
+		host = servletContext.getInitParameter("DBHOST");
+		dbname = servletContext.getInitParameter("DBNAME");
 		username = servletContext.getInitParameter("DBUSERNAME");
 		password = servletContext.getInitParameter("DBPASSWORD");
+		//jdbc:mysql://localhost:3306/student_courses
+		dburl = "jdbc:mysql://" + host + ":3306/" + dbname;
 		this.courseStudentService = new CourseStudentServiceImpl(dburl, username, password);		
 	}
-	
+
 	@GET
-	@Path("/helloworld")
+	@Path("/")
 	@Produces("text/html")
 	public String helloWorld() {
-		System.out.println("Inside helloworld");
 		System.out.println("DB creds are:");
 		System.out.println("DBURL:" + dburl);
 		System.out.println("DBUsername:" + username);
 		System.out.println("DBPassword:" + password);		
-		return "Hello world " + dburl + " " + username + " " + password;		
+		return "DB Creds:" + dburl + " " + username + " " + password + "\n";		
 	}
-		
+
+	@POST
+	@Path("/courses")
+	@Produces("application/xml")
+	public NewCourse addCourse(NewCourse courseToAdd) throws Exception {
+		String name = courseToAdd.getName();
+		String courseNum = courseToAdd.getCourseNum();
+		System.out.println("Name:" + name + " CourseNum:" + courseNum);
+		NewCourse newCourse = this.courseStudentService.addCourse(courseToAdd);
+		return newCourse;
+	}
+
+	@GET
+	@Path("/courses/{course_id}")
+	@Produces("application/xml")
+	public NewCourse getCourse(@PathParam("course_id") int courseId) throws Exception {
+		NewCourse newCourse = this.courseStudentService.getCourse_correct(courseId);
+		return newCourse;
+	}
+	
+	@GET
+	@Path("/courses")
+	@Produces("application/xml")
+	public NewCourse getCourseWithQueryParam(@QueryParam("course_id") int courseId) throws Exception {
+		NewCourse newCourse = this.courseStudentService.getCourse_correct(courseId);
+		return newCourse;
+	}
+
+	@PUT
+	@Path("/courses/{courseId}")
+	@Produces("application/xml")
+	public NewCourse updateCourse(@PathParam("courseId") int courseId) throws Exception {
+		System.out.println("Course to update id:" + courseId);
+		NewCourse updatedCourse = new NewCourse(); 
+		// Make call to Service to update the course
+		// Update course
+		return updatedCourse;
+	}
+
+	@DELETE
+	@Path("/courses/{courseId}")
+	public Response deleteCourse(@PathParam("courseId") int courseId) throws Exception {
+		System.out.println("Course to delete id:" + courseId);
+		// Make call to Service to update the course
+		return Response.ok().build();
+	}
+
 	@GET
 	@Path("/courses")
 	@Produces("application/xml")
@@ -76,49 +131,6 @@ public class UTCoursesResource {
 	      };	    
 	}
 	
-	@GET
-	@Path("/projects")
-	@Produces("application/xml")
-	public StreamingOutput getAllProjects() throws Exception {
-		//Project heat = new Project();
-		//heat.setName("%23heat");
-				
-		final Projects projects = new Projects();
-		projects.setProjects(new ArrayList<String>());
-		//projects.getProjects().add("%23heat");
-		projects.getProjects().add("%23dox");		
-			    
-	    return new StreamingOutput() {
-	         public void write(OutputStream outputStream) throws IOException, WebApplicationException {
-	            outputCourses(outputStream, projects);
-	         }
-	      };	    
-	}	
-	
-	@GET
-	@Path("/project")
-	@Produces("application/xml")
-	public StreamingOutput getProject() throws Exception {
-		
-		final Project heat = new Project();
-		heat.setName("%23heat");
-		heat.setLink(new ArrayList<String>());
-		heat.getLink().add("l3");
-		heat.getLink().add("l2");		
-		
-		//throw new WebApplicationException();
-		
-		final NotFound notFound = new NotFound();
-		notFound.setError("Project non-existent-project does not exist");
-		
-	    return new StreamingOutput() {
-	         public void write(OutputStream outputStream) throws IOException, WebApplicationException {
-	            outputCourses(outputStream, notFound);
-	         }
-	      };
-	     
-	}		
-	
 	protected void outputCourses(OutputStream os, Courses courses) throws IOException {
 		try { 
 			JAXBContext jaxbContext = JAXBContext.newInstance(Courses.class);
@@ -132,33 +144,7 @@ public class UTCoursesResource {
 		}
 	}
 	
-	protected void outputCourses(OutputStream os, Projects projects) throws IOException {
-		try { 
-			JAXBContext jaxbContext = JAXBContext.newInstance(Projects.class);
-			Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
-	 
-			jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-			jaxbMarshaller.marshal(projects, os);
-		} catch (JAXBException jaxb) {
-			jaxb.printStackTrace();
-			throw new WebApplicationException();
-		}
-	}	
-	
-	protected void outputCourses(OutputStream os, Project project) throws IOException {
-		try { 
-			JAXBContext jaxbContext = JAXBContext.newInstance(Project.class);
-			Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
-	 
-			jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-			jaxbMarshaller.marshal(project, os);
-		} catch (JAXBException jaxb) {
-			jaxb.printStackTrace();
-			throw new WebApplicationException();
-		}
-	}
-	
-	protected void outputCourses(OutputStream os, NotFound notFound) throws IOException {
+	protected void outputNotFound(OutputStream os, NotFound notFound) throws IOException {
 		try { 
 			JAXBContext jaxbContext = JAXBContext.newInstance(NotFound.class);
 			Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
